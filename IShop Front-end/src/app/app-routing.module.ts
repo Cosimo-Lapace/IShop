cae840d2b0dc4e7769d36e315947193507d90d3f
Cosimo Lapace <120408625+Cosimo-Lapace@users.noreply.github.com>
2023-10-10 16:53:10 +0200

    category id

--- IShop Front-end/src/app/app-routing.module.ts
+++ IShop Front-end/src/app/app-routing.module.ts
@@ -3,13 +3,23 @@ import { RouterModule, Routes } from '@angular/router';
 import { ProductListComponent } from './components/product-list/product-list.component';
 import { Error404Component } from './error/error404/error404.component';
 
-
 const routes: Routes = [
+  {
+    path: 'category/:id',
+    component: ProductListComponent,
+  },
+  {
+    path: 'category',
+    component: ProductListComponent,
+  },
   {
     path: '',
     component: ProductListComponent,
   },
-  { path: '*', component: Error404Component},
+  {
+    path: '*',
+  component: Error404Component
+  },
 ];
 
 @NgModule({
