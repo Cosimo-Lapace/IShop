376652058c422985cec5c584de67359ee41b4604
Cosimo Lapace <120408625+Cosimo-Lapace@users.noreply.github.com>
2023-10-11 12:43:27 +0200

    getproductbycategoryId

--- IShop Front-end/src/app/components/product-list/product-list.component.ts
+++ IShop Front-end/src/app/components/product-list/product-list.component.ts
@@ -1,12 +1,11 @@
 import {
   AfterViewChecked,
   Component,
-  ElementRef,
   OnDestroy,
   OnInit,
-  ViewChild,
 } from '@angular/core';
-import { Subscription } from 'rxjs';
+import { ActivatedRoute, Router } from '@angular/router';
+
 
 import { Product } from 'src/app/model/product';
 import { ProductService } from 'src/app/services/product.service';
@@ -20,23 +19,52 @@ export class ProductListComponent
   implements OnInit, AfterViewChecked, OnDestroy
 {
   products: Product[] = [];
-  private ProductsSubscription: Subscription;
 
-  constructor(private productSerive: ProductService) {}
+  currentCategoryId: number;
 
-  ngOnInit(): void {
-    this.ProductsSubscription = this.productSerive
+  constructor(
+    private productSerive: ProductService,
+    private route: ActivatedRoute,
+    private router: Router
+  ) {}
+  getRouteParamsById() {
+    // check id exist
+    const isCategoryId: boolean = this.route.snapshot.paramMap.has('id');
+    console.log(isCategoryId);
+    if (isCategoryId) {
+      this.currentCategoryId = +this.route.snapshot.paramMap.get('id');
+      console.log(this.currentCategoryId);
+    } else {
+      this.currentCategoryId = 1;
+    }
+    this.productSerive.getProductByCategoryId(this.currentCategoryId).subscribe(data => this.products = data);
+
+
+  }
+  getProductList() {
+     this.productSerive
       .getProductList()
       .subscribe((data) => {
         this.products = data;
       });
   }
 
+
+  //lifecycle
+  ngOnInit(): void {
+   this.route.paramMap.subscribe(()=>{
+    if(!this.route.snapshot.paramMap.has('id') || +this.route.snapshot.paramMap.get('id') === 0){
+      this.getProductList()
+    }else{
+      this.getRouteParamsById();
+    }
+
+   })
+  }
+
   ngAfterViewChecked(): void {
     let description = document.querySelectorAll('.description');
     let name = document.querySelectorAll('.name');
-
-
     description.forEach((el, i) => {
       if (el.innerHTML.length > 101) {
         let text = el.innerHTML.slice(0, 101 - 3) + '...';
@@ -50,9 +78,11 @@ export class ProductListComponent
         name[i].innerHTML = nameTextSlice;
       }
     });
+
   }
 
   ngOnDestroy(): void {
-    this.ProductsSubscription.unsubscribe();
+
   }
+
 }
--- IShop Front-end/src/app/services/product.service.ts
+++ IShop Front-end/src/app/services/product.service.ts
@@ -9,15 +9,22 @@ import { Product } from '../model/product';
 })
 export class ProductService {
 
-  private baseUrlApi = "http://localhost:8080/api/products?size=100";
+  private baseUrlApi = "http://localhost:8080/api/products";
 
 
   constructor(private httpClient: HttpClient) { }
 
   getProductList() : Observable<Product[]>{
+
       return this.httpClient.get<GetResponse>(this.baseUrlApi).pipe(
         map(response => response._embedded.products)
       )
+  }
+  getProductByCategoryId(id:number) : Observable<Product[]>
+  {
+    return this.httpClient.get<GetResponse>(`${this.baseUrlApi}/search/findByCategoryId?id=${id}`).pipe(
+      map(response => response._embedded.products)
+    )
   }
 
 
