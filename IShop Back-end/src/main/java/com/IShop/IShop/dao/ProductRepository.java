b4e61bd79f8debdcfdd3723cae748581531d2bfc
Cosimo Lapace <120408625+Cosimo-Lapace@users.noreply.github.com>
2023-10-19 11:41:29 +0200

    documentation by api generate

--- IShop Back-end/src/main/java/com/IShop/IShop/dao/ProductRepository.java
+++ IShop Back-end/src/main/java/com/IShop/IShop/dao/ProductRepository.java
@@ -12,9 +12,15 @@ import org.springframework.web.bind.annotation.CrossOrigin;
 @CrossOrigin("http://localhost:4200")
 public interface ProductRepository extends JpaRepository<Product,Long> {
 
+    //send api for productCategoryID
+    //database:
+    //relation by product and categoryProduct
     //http://localhost:8080/api/products/search/findByCategoryId?id=yzf
     Page<Product> findByCategoryId(@Param("id") Long id, Pageable pageable);
 
+    //send api for search by product Name
+    //database:
+    //search table product column name
     //http://localhost:8080/api/products/search/findByNameContaining?name=yzf
     Page<Product> findByNameContaining(@Param("name") String name,Pageable pageable);
 }
